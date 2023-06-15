72c9b338120887a42cc7e52bd58c9b8be39aa853
OfekLap <Ofek2608@gmail.com>
2023-06-15 03:25:29 +0300

    Added Curios Integration Back

--- src/main/java/com/ofek2608/crafting_on_a_stick/integration/COASCurios.java
+++ src/main/java/com/ofek2608/crafting_on_a_stick/integration/COASCurios.java
@@ -1,18 +1,14 @@
 package com.ofek2608.crafting_on_a_stick.integration;
 
-import com.ofek2608.crafting_on_a_stick.CraftingOnAStick;
 import net.minecraft.MethodsReturnNonnullByDefault;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.wrapper.CombinedInvWrapper;
 import net.minecraftforge.items.wrapper.PlayerInvWrapper;
-//import top.theillusivec4.curios.api.CuriosApi;
-//import top.theillusivec4.curios.api.SlotTypeMessage;
-//import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
-//import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
+import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Optional;
@@ -23,16 +19,10 @@ public class COASCurios {
 	@SuppressWarnings("EmptyMethod") public static void load() {}
 	
 	private static final String MODID = "curios";
-	private static final ResourceLocation CURIO_POCKET_ICON = CraftingOnAStick.loc("item/curios_on_a_stick");
-	private static final String SLOT_ID = CraftingOnAStick.ID;
+	private static final String SLOT_ID = "crafting_on_a_stick";
 	
 	public static boolean hasMod() {
 		return ModList.get().isLoaded(MODID);
-	}
-	
-	static {
-		if (hasMod())
-			Integrator.init();
 	}
 	
 	public static Optional<IItemHandlerModifiable> getCuriosInventory(Player player) {
@@ -50,23 +40,12 @@ public class COASCurios {
 	private static final class Integrator {
 		private Integrator() {}
 		
-		private static void init() {
-//			InterModComms.sendTo(MODID, SlotTypeMessage.REGISTER_TYPE, ()->new SlotTypeMessage.Builder(SLOT_ID)
-//					.icon(CURIO_POCKET_ICON)
-//					.size(1)
-//					.build()
-//			);
-		}
-		
 		private static Optional<IItemHandlerModifiable> getCuriosInventory(Player player) {
-//			Optional<ICuriosItemHandler> itemHandler = CuriosApi.getCuriosHelper().getCuriosHandler(player).resolve();
-//			if (itemHandler.isEmpty())
-//				return Optional.empty();
-//			Optional<ICurioStacksHandler> stackHandler = itemHandler.get().getStacksHandler(SLOT_ID);
-//			if (stackHandler.isEmpty())
-//				return Optional.empty();
-//			return Optional.of(stackHandler.get().getStacks());
-			return Optional.empty();
+			Optional<ICuriosItemHandler> itemHandler = CuriosApi.getCuriosHelper().getCuriosHandler(player).resolve();
+			if (itemHandler.isEmpty())
+				return Optional.empty();
+			Optional<ICurioStacksHandler> stackHandler = itemHandler.get().getStacksHandler(SLOT_ID);
+			return stackHandler.map(ICurioStacksHandler::getStacks);
 		}
 	}
 }
