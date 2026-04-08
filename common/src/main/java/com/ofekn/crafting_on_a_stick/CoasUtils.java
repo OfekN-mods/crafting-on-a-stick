a3629f67a6059fe595fdc6e6a83050d61583ceb3
OfekN_ <ofek2608@gmail.com>
2026-04-08 08:14:10 +0300

    implemented missing method

--- common/src/main/java/com/ofekn/crafting_on_a_stick/CoasUtils.java
+++ common/src/main/java/com/ofekn/crafting_on_a_stick/CoasUtils.java
@@ -1,10 +1,12 @@
 package com.ofekn.crafting_on_a_stick;
 
 import com.ofekn.crafting_on_a_stick.api.Ref;
+import com.ofekn.crafting_on_a_stick.integration.CoasIntegrations;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
@@ -28,7 +30,15 @@ public final class CoasUtils {
     }
 
     public static List<Ref<ItemStack>> getFullInventory(Player player) {
-        //TODO implement
-        return null;
+        List<Ref<ItemStack>> result = new ArrayList<>();
+        var inventory = player.getInventory();
+        Ref.forEveryIndex(
+                inventory::getItem,
+                inventory::setItem,
+                inventory.getContainerSize(),
+                result
+        );
+        CoasIntegrations.CURIOS.getCuriosInventory(player, result);
+        return result;
     }
 }
--- common/src/main/java/com/ofekn/crafting_on_a_stick/integration/CuriosIntegration.java
+++ common/src/main/java/com/ofekn/crafting_on_a_stick/integration/CuriosIntegration.java
@@ -1,6 +1,16 @@
 package com.ofekn.crafting_on_a_stick.integration;
 
+import com.ofekn.crafting_on_a_stick.api.Ref;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+
 public enum CuriosIntegration implements ICuriosIntegration {
     INSTANCE;
 
+    @Override
+    public void getCuriosInventory(Player player, List<Ref<ItemStack>> result) {
+        // TODO implement
+    }
 }
