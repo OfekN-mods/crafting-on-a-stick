bafb319f50875fc7dc837828d62bfd0234d92713
OfekN_ <ofek2608@gmail.com>
2026-04-08 07:00:21 +0300

    @NullMarked

---
+++ common/src/main/java/com/ofekn/crafting_on_a_stick/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.ofekn.crafting_on_a_stick;
+
+import org.jspecify.annotations.NullMarked;
---
+++ fabric/src/main/java/com/ofekn/crafting_on_a_stick/fabric/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.ofekn.crafting_on_a_stick.fabric;
+
+import org.jspecify.annotations.NullMarked;
