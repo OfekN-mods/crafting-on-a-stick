fe3a00df488163a17a3fd9ca61f9b0173932890c
OfekN_ <ofek2608@gmail.com>
2026-04-08 06:01:18 +0300

    initial commit template by jaredlll08

---
+++ common/src/main/java/com/ofekn/crafting_on_a_stick/Constants.java
@@ -0,0 +1,11 @@
+package com.ofekn.crafting_on_a_stick;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "crafting_on_a_stick";
+	public static final String MOD_NAME = "Crafting on a Stick";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/com/ofekn/crafting_on_a_stick/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package com.ofekn.crafting_on_a_stick.platform;
+
+import com.ofekn.crafting_on_a_stick.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
